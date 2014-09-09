b1d30323c66297bcd0eeeb3d3169999d4273abb1
Justin <jyang772@gmail.com>
2014-09-09 11:18:41 -0700

    Update openinventory.h

--- headers/openinventory.h
+++ headers/openinventory.h
@@ -33,7 +33,7 @@ private:
     QString path, username;
     QStringList keys, decrypted_keys;
     QString key_selected;
-    Encoder *r2d2 = new Encoder(this); //this
+    Encoder *r2d2 = new Encoder(); //this
 };
 
 #endif // OPENINVENTORY_H
